06fb22f51cee957ad0ecccbecd35091ef7ccbb58
Maya <mayaasparouhova9015@gmail.com>
2020-08-27 12:48:38 -0600

    I changed Mr. Nice to buk

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,8 +8,8 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
+      { id: 11, name: 'book' },
+      { id: 12, name: 'buk' },
       { id: 13, name: 'Bombasto' },
       { id: 14, name: 'Celeritas' },
       { id: 15, name: 'Magneta' },
